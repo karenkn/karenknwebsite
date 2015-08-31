977c4cdaa03706e0b0ba59e83d23b391b1479d2c
karenkn <karen.k.nan@gmail.com>
2015-08-30 23:12:37 -0400

    changed db to postgresql

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cool = require('cool-ascii-faces');
 var app = express();
-//var pg = require('pg');
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -20,21 +20,17 @@ app.get('/cool', function(request, response) {
 });
 
 
-
-// this is not for mongo
-
-// app.get('/db', function (request, response) {
-//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//     client.query('db.test_table.find()', function(err, result) {
-//       done();
-//       if (err)
-//        { console.error(err); response.send("Error " + err); }
-//       else
-//        { response.render('pages/db', {results: result.rows} ); }
-//     });
-//   });
-// })
-
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
